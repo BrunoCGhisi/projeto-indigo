a41f7aeb6d655e404e875d838ae47370ff204e1d
Bruno Costa Ghisi <brunocostaghisi@gmail.com>
2024-06-20 21:17:33 -0300

    maria joana

--- App.js
+++ App.js
@@ -1,25 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import SignUp from './src/Screens/SignUp';
-import SignIn from './src/Screens/SignIn';
-import Home from './src/Screens/Home';
-import User from './src/Screens/User';
-import UserProfile from './src/Screens/UserProfile';
-import Post from './src/Screens/Post';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import SignUp from "./src/Screens/SignUp";
+import SignIn from "./src/Screens/SignIn";
+import Home from "./src/Screens/Home";
+import User from "./src/Screens/User";
+import UserProfile from "./src/Screens/UserProfile";
+import Post from "./src/Screens/Post";
 
 export default function App() {
   const Stack = createStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='SignIn' >
-        <Stack.Screen name='SignIn' component={SignIn} options={{headerShown: false}} />
-        <Stack.Screen name='SignUp' component={SignUp} options={{headerShown: false}} />
-        <Stack.Screen name="Post" component={Post} options={{headerShown: false}} />
-        <Stack.Screen name='Login' component={Login} options={{headerShown: false}} />
-        
-        <Stack.Screen name='Home' component={Home} options={{headerShown: false}} />
-        <Stack.Screen name="User" component={User} options={{headerShown: false}} />
-        <Stack.Screen name="UserProfile" component={UserProfile} options={{headerShown: false}} />
+      <Stack.Navigator initialRouteName="SignIn">
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Post"
+          component={Post}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="User"
+          component={User}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="UserProfile"
+          component={UserProfile}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
